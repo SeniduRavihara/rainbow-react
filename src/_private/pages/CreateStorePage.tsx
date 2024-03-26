4d9818ebbbdb1c8a07bdddd71603b98c750905eb
Senidu Ravihara <154441350+SeniduRavihara@users.noreply.github.com>
2024-03-26 21:58:45 +0530

    create multiplule photo uploading

--- src/_private/pages/CreateStorePage.tsx
+++ src/_private/pages/CreateStorePage.tsx
@@ -1,86 +1,107 @@
 import ImageSwiper from "../components/ImageSwiper";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { IonIcon } from "@ionic/react";
 import { addOutline } from "ionicons/icons";
 import "./register.css";
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+import { IoArrowBack } from "react-icons/io5";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { storage } from "@/firebase/config";
+import { v4 } from "uuid";
+
+import PlaceholderImage from "../../../public/img/image-gallery.png";
 
 const CreateStorePage = () => {
-  const swiperSlides = [
-    /**
-     * Container element for the create store page.
-     */
-    <div className="">
-      <div className="img-area">
-        <div className="">
-          <div>
-            <img
-              src="/assets/img/image-gallery.png"
-              className="w-10 h-10"
-              alt=""
-            />
-          </div>
-          <p>
-            Select your store images <span>Brower</span>
-          </p>
-        </div>
-      </div>
-    </div>,
-    <div className="">
-      <div className="img-area">
-        <div className="upload-img">
-          <div id="createstoreimg1">
-            <img
-              src="/assets/img/image-gallery.png"
-              className="w-10 h-10"
-              alt=""
-            />
-          </div>
-          <p>
-            Select your store images <span id="storebuttonimages1">Brower</span>
-          </p>
-        </div>
-      </div>
-    </div>,
-    <div className="">
-      <div className="img-area">
-        <div className="">
-          <div>
-            <img
-              src="/assets/img/image-gallery.png"
-              className="w-10 h-10"
-              alt=""
-            />
-          </div>
-          <p>
-            Select your store images <span id="storebuttonimages1">Brower</span>
-          </p>
-        </div>
-      </div>
-    </div>,
-  ];
+  const [title, setTitle] = useState("");
+  const [address, setAddress] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [whatsappNumber, setWhatsappNumber] = useState("");
+  const [tags, setTags] = useState("");
+
+  console.log("TEST", PlaceholderImage);
+
+  const [storeImages, setStoreImages] = useState<
+    Array<{
+      index: number;
+      file: File;
+      imageUrl: null | string;
+    }>
+  >([]);
+  const [storeIcon, setStoreIcon] = useState<File | null>(null);
+
+  useEffect(() => {
+    console.log(storeImages);
+  }, [storeImages]);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    handleUpload();
+  };
+
+  const handleUpload = async () => {
+
+    if (storeImages.length > 0) {
+      try {
+        for (let i = 0; i < storeImages.length; i++) {
+          const file = storeImages[i].file;
+          const fileRef = ref(storage, `store_images/${v4()}`);
+          const snapshot = await uploadBytes(fileRef, file);
+          const photoURL = await getDownloadURL(fileRef);
+
+          // Update storeImages state with the uploaded image's URL
+          setStoreImages((prevImages) => {
+            const updatedImages = [...prevImages];
+
+            // Image exists, update its URL
+            updatedImages[i].imageUrl = photoURL;
+
+            return updatedImages;
+          });
+
+          console.log("Download URL:", photoURL);
+        }
+        console.log("All files uploaded successfully!");
+      } catch (error) {
+        console.error("Error uploading files:", error.message);
+        alert(
+          "An error occurred while uploading files. Please try again later."
+        );
+      }
+    } else {
+      alert("No images to upload!");
+    }
+  };
 
   return (
     <div className="create-store w-full h-screen flex items-center justify-center bg-[#aec5e8]">
+      <Button
+        onClick={handleUpload}
+        className="absolute top-5 left-5 w-[100px] h-[50px]"
+      >
+        UPLOAD
+      </Button>
+
       <Card className="w-[80%] h-[85%] p-10 flex flex-col gap-10">
         <CardHeader>
-          <CardTitle className="-mt-10">
-            <h1 className="text-center text-4xl font-bold text-[#005eff]">
-              Create Store
-            </h1>
+          <CardTitle className="-mt-10 text-center text-4xl font-bold text-[#005eff]">
+            Create Store
           </CardTitle>
-          {/* <CardDescription>Card Description</CardDescription> */}
         </CardHeader>
         <CardContent>
+          <Button variant="outline" asChild className="absolute top-5 left-5">
+            <Link to="/">
+              <IoArrowBack />
+            </Link>
+          </Button>
+
           <div className="flex -mt-10 gap-20">
             <div className="-mt-10">
-              <ImageSwiper swiperSlides={swiperSlides} />
+              <ImageSwiper
+                setStoreImages={setStoreImages}
+                storeImages={storeImages}
+              />
             </div>
 
             <div className="form-conten flex flex-col">
@@ -89,6 +110,14 @@ const CreateStorePage = () => {
                 id="logo-content"
                 style={{ display: "flex" }}
               >
+                <input
+                  id={`iconInput`}
+                  type="file"
+                  accept="image/*"
+                  onChange={(e) => setStoreIcon(e.target.files[0])}
+                  required
+                  className="hidden"
+                />
                 <button
                   className="photo-add-button"
                   id="logo-button"
@@ -97,59 +126,75 @@ const CreateStorePage = () => {
                   <IonIcon icon={addOutline}></IonIcon>
                 </button>
                 <div id="previewImagelogo"></div>
-                <p className=" text-center">
-                  Select your logo <span id="storebuttonlogo">Brower</span>
+                <p className="text-center">
+                  Select your logo{" "}
+                  <label
+                    htmlFor="iconInput"
+                    className="text-blue-500 cursor-pointer"
+                  >
+                    Brower
+                  </label>
                 </p>
               </div>
 
-              <form>
+              <form onSubmit={handleSubmit}>
                 <div className="input-form grid grid-cols-2">
                   <input
                     type="text"
-                    name="title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
                     required
                     placeholder=" title"
+                    className="p-[1rem] text-lg m-[10px] border-2 border-[#a7a7a7] rounded-xl focus:outline-blue-400"
                   />
 
                   <input
                     type="text"
-                    name="address"
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
                     required
-                    placeholder=" address"
+                    placeholder="address"
+                    className="p-[1rem] text-lg m-[10px] border-2 border-[#a7a7a7] rounded-xl focus:outline-blue-400"
                   />
 
                   <input
                     type="text"
-                    name="mobile"
+                    value={phoneNumber}
+                    onChange={(e) => setPhoneNumber(e.target.value)}
                     required
                     placeholder="Phone number"
+                    className="p-[1rem] text-lg m-[10px] border-2 border-[#a7a7a7] rounded-xl focus:outline-blue-400"
                   />
 
                   <input
                     type="text"
-                    name="whats_number"
+                    value={whatsappNumber}
+                    onChange={(e) => setWhatsappNumber(e.target.value)}
                     required
                     placeholder="Whatsapp number"
+                    className="p-[1rem] text-lg m-[10px] border-2 border-[#a7a7a7] rounded-xl focus:outline-blue-400"
                   />
 
-                  <input type="text" name="tag" required placeholder="Tag" />
+                  <input
+                    type="text"
+                    value={tags}
+                    onChange={(e) => setTags(e.target.value)}
+                    required
+                    placeholder="Tag"
+                    className="p-[1rem] text-lg m-[10px] border-2 border-[#a7a7a7] rounded-xl focus:outline-blue-400"
+                  />
 
                   <input
                     type="submit"
                     value="Submit"
-                    id="subbtn"
-                    className=" flex items-center justify-center p-3 font-xl text-white bg-[#0c86ac]"
+                    className=" text-xl m-[10px] rounded-xl flex items-center justify-center p-3 text-white bg-[#0c86ac]"
                   />
                 </div>
               </form>
             </div>
           </div>
         </CardContent>
-        {/* <CardFooter>
-          <p>Card Footer</p>
-        </CardFooter> */}
       </Card>
-      {/* <h1 className="text-center">Create Store</h1> */}
     </div>
   );
 };
