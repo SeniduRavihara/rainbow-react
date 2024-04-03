81fe11799ec70220f7e96ea4c103d7e3d11ab1b4
Senidu Ravihara <154441350+SeniduRavihara@users.noreply.github.com>
2024-04-03 22:53:58 +0530

    add firebase clou functions

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { connectAuthEmulator, getAuth, GoogleAuthProvider } from "firebase/auth";
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
+import { connectStorageEmulator, getStorage } from "firebase/storage";
+import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,3 +23,21 @@ export const db = getFirestore(app);
 export const storage = getStorage(app);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
+
+
+// export const db = getFirestore();
+// connectFirestoreEmulator(db, "127.0.0.1", 8080);
+// export const storage = getStorage();
+// connectStorageEmulator(storage, "127.0.0.1", 9199);
+// export const auth = getAuth();
+// connectAuthEmulator(auth, "http://127.0.0.1:9099");
+// export const provider = new GoogleAuthProvider();
+
+// if (process.env.NODE_ENV === "development") {
+//   const functions = getFunctions(app);
+//   connectFunctionsEmulator(functions, "127.0.0.1", 5001);
+// }
+
+
+// const functions = getFunctions(getApp());
+// connectFunctionsEmulator(functions, "127.0.0.1", 5001);
