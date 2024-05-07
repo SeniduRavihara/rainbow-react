ea372ccfb13cc9e8ef4e0d22a62463c7d3ad0407
Senidu Ravihara <154441350+SeniduRavihara@users.noreply.github.com>
2024-05-07 19:47:20 +0530

    ui update

--- src/_public/components/we-are-hiring/Section3.tsx
+++ src/_public/components/we-are-hiring/Section3.tsx
@@ -2,17 +2,17 @@ import { Button } from "@/components/ui/button"
 
 const Section3 = () => {
   return (
-    <div className="w-full flex flex-col items-center justify-center mt-10">
+    <div className="w-full flex flex-col items-center justify-center mt-28">
       <div>
-        <h2 className="text-center font-semibold text-2xl">
+        <h2 className="text-center font-bold text-2xl md:text-3xl">
           Current Job vacancies at srilanka Business
         </h2>
-        <h3 className="text-center text-2xl">
+        <h3 className="text-center text-xl md:text-2xl">
           See where you fit in Search jobs by location or team
         </h3>
       </div>
 
-      <Button className="mt-4">Contact Us</Button>
+      <Button className="mt-4 px-20 rounded-xl">Contact Us</Button>
     </div>
   );
 }
