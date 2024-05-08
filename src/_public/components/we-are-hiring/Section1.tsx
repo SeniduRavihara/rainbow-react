63461539edd554c22f179b6ac7c62a7acd601c3e
Senidu Ravihara <154441350+SeniduRavihara@users.noreply.github.com>
2024-05-08 14:26:05 +0530

    add we are hiring page contact data

--- src/_public/components/we-are-hiring/Section1.tsx
+++ src/_public/components/we-are-hiring/Section1.tsx
@@ -2,6 +2,12 @@ import { Button } from "@/components/ui/button";
 import { FaArrowRight } from "react-icons/fa";
 
 const Section1 = () => {
+  const handleClickJoinOutTeam = () => {
+    window.open(
+      `https://wa.me/715335640?text=I%27m%20interested%20in%20joining%20your%20team%20at%20Srilanka%20Business.%20Could%20you%20please%20provide%20me%20with%20more%20details%20about%20the%20available%20positions%20and%20the%20application%20process%3F`
+    );
+  };
+
   return (
     <div className="flex flex-col-reverse md:flex-row justify-between items-center md:px-10">
       <div className="space-y-3 mt-10 md:mt-0">
@@ -11,7 +17,7 @@ const Section1 = () => {
         <h3 className="text-sm font-medium">
           Largest Business Directory in srilanka
         </h3>
-        <Button>
+        <Button onClick={handleClickJoinOutTeam}>
           Join Our Team
           <FaArrowRight className="mt-1 ml-2" />
         </Button>
@@ -26,5 +32,5 @@ const Section1 = () => {
       </div>
     </div>
   );
-}
-export default Section1
+};
+export default Section1;
